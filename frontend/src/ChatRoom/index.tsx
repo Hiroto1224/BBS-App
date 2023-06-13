65b030e34e316e6963fffe8ff870681aba073304
Hiroto <hiroto240020@gmail.com>
2023-06-13 20:55:55 +0900

    Create ChatRoom

---
+++ frontend/src/ChatRoom/index.tsx
@@ -0,0 +1,18 @@
+import { Typography, Toolbar} from '@mui/material'
+import React from 'react'
+
+
+const ChatRoom = () => {
+
+    return (
+        <Toolbar>
+            <Typography variant="h6" component="div" style={{flexGrow: "1"}}>
+                Test
+            </Typography>
+        </Toolbar>
+    )
+}
+
+
+
+export default ChatRoom;
