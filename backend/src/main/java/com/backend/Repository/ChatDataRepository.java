702c2d90effb61e2ff4f893f0e9866b0b4279b5e
Hiroto <hiroto240020@gmail.com>
2023-06-13 20:09:46 +0900

    Wip

--- backend/src/main/java/com/backend/Repository/ChatDataRepository.java
+++
@@ -1,7 +0,0 @@
-package com.backend.Repository;
-
-import com.backend.Model.ChatData;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ChatDataRepository extends MongoRepository<ChatData,String > {
-}
--- backend/src/main/java/com/backend/Repository/RoomDataRepository.java
+++
@@ -1,7 +0,0 @@
-package com.backend.Repository;
-
-import com.backend.Model.RoomData;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface RoomDataRepository extends MongoRepository<RoomData,String> {
-}
