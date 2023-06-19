2c44b9b9bd232f055a57bf719b285d240778784e
Hiroto <hiroto240020@gmail.com>
2023-06-19 16:59:00 +0900

    Wip

--- frontend/src/ChatRoom/index.tsx
+++ frontend/src/ChatRoom/index.tsx
@@ -7,16 +7,39 @@ import {
     MessageList
 }
     from '@chatscope/chat-ui-kit-react'
-import { SideBar } from './SideBar'
+// import { SideBar } from './SideBar'
+import useSWR from 'swr'
+
+const fetcher = (url:string): Promise<any[]> => fetch(url,{method: 'Get', mode: "cors"}
+).then(res => res.json())
 
 const ChatRoom = () => {
 
     // const viewTodos:string[] = [ 'test', 'test2', 'test2', 'test2', 'test2', 'test2', 'test2', 'test2', 'test2', 'test2', 'test2', 'test2', 'test2', 'test2', 'test2', 'test2', 'test2', 'test2', 'test2', 'test2', 'test2' ]
+    const { data: roomData, error, isLoading } = useSWR('http://localhost:8080/api/v1/roomData/', fetcher)
+
+    const { data: chatData} = useSWR(`http://localhost:8080/api/v1/chatData`, fetcher)
+
+   /* const {
+        data: userData,
+        error: otherError
+    } = useSWR(chatData ? ['http://localhost:8080/api/v1/users/' + chatData.at(1).sendUserId] : null, fetcher)
+*/
+
+    console.log(roomData)
+    if(!roomData) return <></>
+    if(!chatData) return <></>
+
+
+    const data = roomData.map(data => {
+        return chatData.filter(chatData => data.id === chatData.roomId)
+    })
+    console.log(data)
 
     return (
         <div style={{ position: "relative",height:"569px"}}>
             <MainContainer responsive>
-                <SideBar />
+                {/*<SideBar />*/}
                 <ChatContainer>
                     <ConversationHeader>
                         <ConversationHeader.Content userName="Test" info="Active 10 mins ago" />
